8a89275927516239261a597409f4fe80177020b8
ahmedmohmd <er.ahmed.3301@gmail.com>
2024-03-10 11:30:23 +0200

    Setup basic project with basic needed packages

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}...`);
+});
